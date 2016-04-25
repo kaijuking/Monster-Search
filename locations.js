192b41d28d8b8f0472735488cd2c1be00ba8edb0
Mike Field <field1062000@yahoo.com>
2016-04-25 16:22:08 -0700

    I added some default data to the locations and monsters js files.

---
+++ locations.js
@@ -0,0 +1,40 @@
+var locations = [
+  {
+    id: '1',
+    cityName: 'Tokyo',
+    alternateCityName: '東京',
+    stateName: 'Tokyo',
+    alternateStateName: '東京',
+    countryName: 'Japan',
+    alternateCountryName: '日本',
+    longitude: 35.652832,
+    latitude: 139.839478,
+    numberOfMonsters = 1
+  },
+  {
+    id: '2',
+    cityName: 'Sapparo',
+    alternateCityName: '札幌市',
+    stateName: 'Hokkaido',
+    alternateStateName: '北海道',
+    countryName: 'Japan',
+    alternateCountryName: '日本',
+    longitude: 43.0621,
+    latitude: 141.3544,
+    numberOfMonsters = 1
+  },
+  {
+    id: '3',
+    cityName: 'Osaka',
+    alternateCityName: '大阪',
+    stateName: 'Osaka',
+    alternateStateName: '大阪',
+    countryName: 'Japan',
+    alternateCountryName: '日本',
+    longitude: 34.6937,
+    latitude: 135.5022,
+    numberOfMonsters = 1
+  }
+]
+
+module.exports = locations;
