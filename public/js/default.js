cb11ecc59ca61f7947311c2c50ff6c4b0ecfd475
Mike Field <field1062000@yahoo.com>
2016-04-25 16:33:11 -0700

    I updated the layout of the index file as well as the css and front-end js file.

--- public/js/default.js
+++ public/js/default.js
@@ -6,9 +6,26 @@ function initMap() {
   console.log('inside the initMap function');
 
   var map;
+  var tokyo = {lat: 35.6895, lng: 139.6917};
+  var sapporo = {lat: 43.0621, lng: 141.3544};
 
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
+    center: tokyo,
     zoom: 8
   });
+
+  var tokyo = new google.maps.Marker({
+    position: tokyo,
+    map: map,
+    title: 'Tokyo'
+  });
+
+  var sapporo = new google.maps.Marker({
+    position: sapporo,
+    map: map,
+    title: 'Sapparo'
+  });
+
+  tokyo.setMap(map);
+  sapporo.setMap(map);
 }
